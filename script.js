24ca7ed8e1e7cdff6ee64736b81ee25c5112573c
Andrii Umanets <AND@gmail.com>
2025-01-04 15:20:53 +0200

    Products rendering functionality was updated:

--- script.js
+++ script.js
@@ -1,33 +1,51 @@
 fetch("https://fakestoreapi.in/api/products")
 .then(res => res.json())
 .then(res => {
-    console.log(res.products[0])
-    for(let i = 0; i < 5; i++) {
-        const productsContainer = document.querySelector(".productsContainer")    
-        const product = document.createElement("li");
-        product.classList.add('product');
-        productsContainer.appendChild(product);
-    
-        const productDescription = document.createElement("div");
-        productDescription.classList.add('productDescription')
-        const productPrice = document.createElement("div");
-        productPrice.classList.add('productPrice');
-        product.append(productDescription, productPrice)
-    
-        const image = document.createElement('img');
-        image.src = res.products[i].image;
-        image.alt = "IMAGE"
-        const title = document.createElement('h4');
-        title.innerHTML = res.products[i].title;
-        const description = document.createElement('p');
-        description.innerHTML = res.products[i].description;
-        productDescription.append(image, title, description)
+    console.log(res.products)
 
-        const price = document.createElement('span');
-        price.innerHTML = `${res.products[i].price} $`;
-        const button = document.createElement('button');
-        button.innerHTML = "Add to cart"
-        productPrice.append(price, button)
-    }    
+    function renderProducts(i) {
+        if (i < res.products.length) {
+            const productsContainer = document.querySelector(".productsContainer")    
+            const product = document.createElement("li");
+            product.classList.add('product');
+            productsContainer.appendChild(product);
+        
+            const productDescription = document.createElement("div");
+            productDescription.classList.add('productDescription')
+            const productPrice = document.createElement("div");
+            productPrice.classList.add('productPrice');
+            product.append(productDescription, productPrice)
+        
+            const image = document.createElement('img');
+            image.src = res.products[i].image;
+            image.alt = "IMAGE"
+            const title = document.createElement('h4');
+            title.innerHTML = res.products[i].title;
+            const description = document.createElement('p');
+            description.innerHTML = res.products[i].description;
+            productDescription.append(image, title, description)
+    
+            const price = document.createElement('span');
+            price.innerHTML = `${res.products[i].price} $`;
+            const button = document.createElement('button');
+            button.innerHTML = "Add to cart"
+            productPrice.append(price, button)
+        }
+        
+    }
+    for(let i = 0; i < 6; i++) {
+        renderProducts(i)
+    }
+
+    const showMore = document.querySelector(".moreProducts");
+    let counterMin = 6;
+    let counterMax = 12;
+    showMore.addEventListener('click', () => {
+        for(let i = counterMin; i < counterMax; i++) {
+            renderProducts(i)
+        }   
+        counterMin+=6; 
+        counterMax+=6;
+    })   
 
 })
