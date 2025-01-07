ce49b5dcbedb2b593f2b03b9fa74272118557163
Andrii Umanets <AND@gmail.com>
2025-01-08 01:59:08 +0200

    Cart was updated, calculation of items in cart was created

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ fetch("https://fakestoreapi.in/api/products")
     let cartArray = [];
     const cartListContainer = document.querySelector('.cartListContainer');
     cartListContainer.classList.add('cartListContainerEmpty');
+    const cartList = document.querySelector(".cartList");
+    const cartEmptyTitle = document.querySelector('.cartEmptyTitle');
+    const total = document.querySelector('.total');
+
+    let totalValue = 0;
 
     function renderProducts(i) {
         if (i < res.products.length) {
@@ -33,52 +38,81 @@ fetch("https://fakestoreapi.in/api/products")
             const button = document.createElement('button');
             button.innerHTML = "Add to cart";
             button.addEventListener('click', () => {
-                cartRender(i)
-                cartArray.push(i)
-                console.log(cartArray)
-                if(cartArray.length > 0) {
-                    cartListContainer.classList.remove('cartListContainerEmpty')
-                } else {
-                    cartListContainer.classList.add('cartListContainerEmpty');
-                }
+                if(!cartArray.includes(res.products[i])) {
+                    cartArray.push(res.products[i])
+                    cartList.innerHTML = '';
+                    totalValue = 0;
+                    cartArray.map((product) => {
+                        cartRender(product)
+                        totalValue += product.price;
+                    })
+                    cartListContainer.classList.remove('cartListContainerEmpty');
+                    cartEmptyTitle.innerHTML = "";
+                    total.innerHTML = `${totalValue}$`;
+                }                
             })
             productPrice.append(price, button)
         }        
     }
 
-    function cartRender(index) {
-       const cartList = document.querySelector(".cartList");
-       cartList.classList.add('cartList');
-       const cartListLi = document.createElement('li');
-       cartList.appendChild(cartListLi)
-
-       const cartListItem = document.createElement('div');
-       cartListItem.classList.add('cartListItem');
-       const cartListItemImage = document.createElement('img');
-       cartListItemImage.classList.add('cartListItemImage');
-       cartListItemImage.src = res.products[index].image;
-       const cartListItemSpan = document.createElement('span');
-       cartListItemSpan.innerHTML = res.products[index].title;
-       cartListItem.append(cartListItemImage, cartListItemSpan);
-
-       const cartListPrice = document.createElement('div');
-       cartListPrice.classList.add('cartListPrice');
-       const cartListPriceText = document.createElement('h4');
-       cartListPriceText.innerHTML = `${res.products[index].price}$`;
-       cartListPrice.appendChild(cartListPriceText);
-
-       const cartListQuantity = document.createElement('div');
-       cartListQuantity.classList.add('cartListQuantity');
-       const cartListQuantityInput = document.createElement('input');
-       cartListQuantityInput.type = "number";
-       const cartListQuantityButton = document.createElement("button");
-       cartListQuantityButton.innerHTML = "Remove";
-       cartListQuantityButton.addEventListener('click', () => {
-        cartListLi.remove();
-       })
-       cartListQuantity.append(cartListQuantityInput, cartListQuantityButton)
-
-       cartListLi.append(cartListItem, cartListPrice, cartListQuantity)
+    function cartRender(product) {
+        
+            cartList.classList.add('cartList');
+            const cartListLi = document.createElement('li');
+            cartList.appendChild(cartListLi)
+     
+            const cartListItem = document.createElement('div');
+            cartListItem.classList.add('cartListItem');
+            const cartListItemImage = document.createElement('img');
+            cartListItemImage.classList.add('cartListItemImage');
+            cartListItemImage.src = product.image;
+            const cartListItemSpan = document.createElement('span');
+            cartListItemSpan.innerHTML = product.title;
+            cartListItem.append(cartListItemImage, cartListItemSpan);
+     
+            const cartListPrice = document.createElement('div');
+            cartListPrice.classList.add('cartListPrice');
+            const cartListPriceText = document.createElement('h4');
+            cartListPriceText.innerHTML = `${product.price}$`;
+            cartListPrice.appendChild(cartListPriceText);
+     
+            const cartListQuantity = document.createElement('div');
+            cartListQuantity.classList.add('cartListQuantity');
+            const cartListQuantityInput = document.createElement('input');
+            cartListQuantityInput.type = "number";
+            cartListQuantityInput.min = "1";
+            cartListQuantityInput.value = 1;
+            cartListQuantityInput.addEventListener('change', () => {
+                totalValue = 0;
+                itemsQuantity = cartListQuantityInput.value;
+                cartArray.map((product) => {
+                   totalValue += product.price;
+               })
+               totalValue = totalValue + product.price * itemsQuantity - product.price;
+               total.innerHTML = `${totalValue}$`;
+            })
+            const cartListQuantityButton = document.createElement("button");
+            cartListQuantityButton.innerHTML = "Remove";
+            cartListQuantityButton.addEventListener('click', () => {
+             cartListLi.remove();
+             cartArray = cartArray.filter((item) => {
+                return item != product;
+             })
+             cartList.innerHTML = ''
+             totalValue = 0;
+             cartArray.map((product) => {
+                cartRender(product);
+                totalValue += product.price;
+            })
+             if(cartArray.length == 0) {
+                cartListContainer.classList.add('cartListContainerEmpty');
+                cartEmptyTitle.innerHTML = "Your cart is empty";
+             }
+             total.innerHTML = `${totalValue}$`;
+            })
+            cartListQuantity.append(cartListQuantityInput, cartListQuantityButton)
+     
+            cartListLi.append(cartListItem, cartListPrice, cartListQuantity)
        
     }
 
